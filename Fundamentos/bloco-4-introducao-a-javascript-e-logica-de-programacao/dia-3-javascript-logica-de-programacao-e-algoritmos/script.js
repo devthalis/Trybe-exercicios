d211e52334561477f6dbabf8434ef69e4e8392d0
Thalis Venancio <devthalis@gmail.com>
2022-06-25 15:07:32 -0300

    Exercicios resolvidos

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,39 @@
+// let total = 1
+
+// for(let fatorial = 1; fatorial <= 10; fatorial += 1){
+
+//     total *= fatorial
+// }
+// console.log(total)
+
+// let word = "tryber"
+
+// let newString = ''
+
+// for(i = word.length - 1; i >= 0; i -= 1) {
+//     newString += word[i];
+// }
+// console.log(newString);
+
+//
+//Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+// let maior = array[0]
+// // let menor = array[0]
+
+
+// for(let index = 0; index < array.length; index += 1){
+
+//     if(array[index].length > maior.length){
+//         maior = array[index]
+//     }
+ 
+// }
+
+// console.log(maior);
+
+
+
+
+
